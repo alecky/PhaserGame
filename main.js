905124d6a36a26862cd91ce99fbea3eab7e6a8b0
Alex Riolo <alex@quackous.com>
2015-10-28 22:12:33 +0000

    added walls and properties for the wall. restart on player out of game

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ var mainState = {
         // This function will be executed at the beginning 
         // That's where we load the game's assets
         game.load.image('player', 'assets/player.png');
+        game.load.image('wallV', 'assets/wallVertical.png');
+        game.load.image('wallH', 'assets/wallHorizontal.png');
     },
     create: function () {
         'use strict';
@@ -24,12 +26,18 @@ var mainState = {
         // Add vertical gravity to the player
         this.player.body.gravity.y = 500;
         this.cursor = game.input.keyboard.createCursorKeys();
+        this.createWorld();
     },
     update: function () {
         'use strict';
+        // Tell Phaser that the player and the walls should collide
+        game.physics.arcade.collide(this.player, this.walls);
         // This function is called 60 times per second 
         // It contains the game's logic
         this.movePlayer();
+        if (!this.player.inWorld) {
+            this.playerDie();
+        }
     },
     movePlayer: function () {
         'use strict';
@@ -50,7 +58,38 @@ var mainState = {
             // Move the player upward (jump) 
             this.player.body.velocity.y = -320;
         }
+    },
+    
+    createWorld: function () {
+        'use strict';
+        // Create our wall group with Arcade physics 
+        this.walls = game.add.group();
+        this.walls.enableBody = true;
+
+        // Create the 10 walls
+        game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left 
+        game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
+        
+        game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top left 
+        game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right 
+        game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left 
+        game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+        game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left 
+        game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+        var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+        middleTop.scale.setTo(1.5, 1);
+        var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+        middleBottom.scale.setTo(1.5, 1);
+          
+        // Set all the walls to be immovable
+        this.walls.setAll('body.immovable', true);
+    },
+    
+    playerDie: function () {
+        'use strict';
+        game.state.start('main');
     }
+
     // And here we will later add some of our own functions
 };
 
